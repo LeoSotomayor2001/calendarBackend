36f4ceb8393c762832d85ddd5abca8c599e92631
Leodomi <Leodomi15@gmail.com>
2023-03-06 20:43:15 -0400

    app uploaded

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ app.use(express.json());
 app.use('/api/auth', require('./routes/auth')); 
 app.use('/api/events', require('./routes/events')); 
 
+app.get('*', (req,res)=>{
+
+    res.sendFile(__dirname+ '/public/index.html')
+
+});
+
 
 //*escuchar peticiones
 
